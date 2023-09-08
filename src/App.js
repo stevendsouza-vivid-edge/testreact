0f5196aab188613cc19425d3da5a4b45d8a11efe
stevendsouza-vivid-edge <142300447+stevendsouza-vivid-edge@users.noreply.github.com>
2023-09-08 13:00:23 +0530

    Push modified data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Switch from '@mui/material/Switch';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -7,44 +7,100 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { TreeView } from '@mui/x-tree-view/TreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
-import { sampleFromStackOverflowQuestion, seasons } from "./sampleData";
+import { DataTreeJSON } from "./sampleData";
+import Grid from '@mui/material/Grid';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 
-const getTreeItemsFromData = treeItems => {
-  return treeItems.map(treeItemData => {
-    let children = undefined;
-    if (treeItemData.children && treeItemData.children.length > 0) {
-      children = getTreeItemsFromData(treeItemData.children);
-    }
-    return (
-      <TreeItem
-        key={treeItemData.id}
-        nodeId={treeItemData.id}
-        label={treeItemData.name}
-        children={children}
-      />
-    );
-  });
-};
-const DataTreeView = ({ treeItems }) => {
-  return (
-    <TreeView
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-    >
-      {getTreeItemsFromData(treeItems)}
-    </TreeView>
-  );
-};
+class App extends React.Component { 
+	
+	constructor(props) {
+		super(props);
+		debugger;
+		this.products = DataTreeJSON.elements[0].jsondata.elements;
+		this.state = {
+		  currentItem: { ...this.products},
+		};
+		this.loadDataGrid = this.loadDataGrid.bind(this);
+	  }
 
-function App() {  
-return (
-    <div className="App">
-      <DataTreeView treeItems={sampleFromStackOverflowQuestion} />
-      <br />
-      <DataTreeView treeItems={seasons} />
-    </div>
-  );
-}  
+	loadDataGrid = (treeItemData) => {	
+		debugger;
+		this.setState ({
+			currentItem: { ...treeItemData },
+		  });
+	};
+
+    getTreeItemsFromData = treeItems => {
+	return treeItems.map(treeItemData => {
+	  let children = undefined;
+	  if (treeItemData.children && treeItemData.children.length > 0) {
+		children = this.getTreeItemsFromData(treeItemData.children);
+	  }
+	  
+	  return (
+		<TreeItem
+		  key={treeItemData.id}
+		  nodeId={treeItemData.id}
+		  children={children}
+		  label={
+			<div onClick={event => event.stopPropagation()}>
+			 <div onClick={() => { this.loadDataGrid(treeItemData.jsondata.elements);}}
+        >{treeItemData.label}</div>
+			</div>
+		  }
+		/>
+	  );
+	});
+  };
   
+   
+    DataTreeView = ({ treeItems }) => {
+	return (
+	  <TreeView
+		defaultCollapseIcon={<ExpandMoreIcon />}
+		defaultExpandIcon={<ChevronRightIcon />}
+	  >
+		{this.getTreeItemsFromData(treeItems)}
+	  </TreeView>
+	);
+  };
+
+
+
+	render (){
+		const { currentItem } = this.state;
+	return (
+		<Box sx={{ height: 400, mt: 1 }}>
+
+		<Grid container spacing={2}>
+			<Grid item xs={8} md={4}>
+			<this.DataTreeView treeItems={DataTreeJSON.elements	} />
+			</Grid>
+			<Grid item xs={8} md={8}>
+			
+          <div className="name">{currentItem.id}</div>
+          <div className="price">{currentItem.name}</div>
+		  <DataGrid
+        rows={currentItem.rows}
+        columns={currentItem.columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+      />
+       
+        
+			</Grid>
+		</Grid>
+		</Box>	
+	);
+	} ;
+}
+  
+
 export default App;  
--- src/sampleData.js
+++ src/sampleData.js
@@ -1,29 +1,176 @@
 import { v4 as uuid } from 'uuid';
-
-export const sampleFromStackOverflowQuestion = [
-  {
-    id: uuid(),
-    name: "English",
-    children: [
-      {
-        id: uuid(),
-        name: "Spring",
-        children: []
-      }
+export const DataTreeJSON =
+{
+    type: "dataTree",
+    properties: {
+        expanded: "Y/N" //Y - if the tree should render with all nodes expanded. N(default)- to render with all nodes collapsed.
+    },
+    elements: [//Data of the tree nodes. This sample shows three nodes. Add as many as required.
+        {
+            label: "Table Data",
+            id: uuid(),
+            type: "table",
+            image: "none",// Should be from a predefined set of images.
+            jsondata:
+              {
+                  type: "table",
+                  properties: {
+                      header: "My New Table",
+                      keyName: "id",
+                      pageSizeOptions: [ // Pagination options to be applied for the table. All will be shown by default. Pagination will be hidden if this key is missing or empty.
+                          20,
+                          40,
+                          60
+                      ],
+                      defaultPageSize: "20" //Will be ignored if pageSizeOptions is not provided. Pagesize will default to All if this key is missing or empty.
+                  },
+                  elements:{
+                    columns:[
+                    { field: 'id', headerName: 'ID', width: 90 },
+                    {
+                      field: 'firstName',
+                      headerName: 'First name',
+                      width: 150,
+                      editable: true,
+                    },
+                    {
+                      field: 'lastName',
+                      headerName: 'Last name',
+                      width: 150,
+                      editable: true,
+                    },
+                    {
+                      field: 'age',
+                      headerName: 'Age',
+                      type: 'number',
+                      width: 110,
+                      editable: true,
+                    },
+                    {
+                      field: 'fullName',
+                      headerName: 'Full name',
+                      description: 'This column has a value getter and is not sortable.',
+                      sortable: false,
+                      width: 160,
+                    },
+                  ], rows: [
+                    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+                    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+                    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+                    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+                    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+                    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+                    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+                    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+                    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+                  ]
+                }
+              },
+            children: [ {
+              label: "Form",
+              id: uuid(),
+              type: "form",
+              image: "none",// Should be from a predefined set of images.
+              jsondata:{// First element to render within the Accordion.
+                type: "form",
+                properties: {
+                    columns: 3 // Maximum No. of columns to layout the form feilds into.
+                },
+                elements: [
+                    {
+                        label: "Label of the feild",
+                        value: "Value of the feild",
+                        type: "OutputText", // This will be OutputText for all the forms. Unless we come across a different form feild.
+                        required: "Y/N" //Y - if required indicator is to be shown. Omit this key or pass N if not a required feild.
+                    },
+                    {
+                        label: "Label of the feild",
+                        value: "Value of the feild",
+                        type: "OutputText", // This will be OutputText for all the forms. Unless we come across a different form feild.
+                        required: "Y/N" //Y - if required indicator is to be shown. Omit this key or pass N if not a required feild.
+                    },
+                    {
+                        label: "Label of the feild",
+                        value: "Value of the feild",
+                        type: "OutputText", // This will be OutputText for all the forms. Unless we come across a different form feild.
+                        required: "Y/N" //Y - if required indicator is to be shown. Omit this key or pass N if not a required feild.
+                    }
+                ]
+            }
+              },
+              
+              {label: "Table Data 1",
+              id: uuid(),
+              type: "table",
+              image: "none",// Should be from a predefined set of images.
+              jsondata:{
+                type: "table",
+                properties: {
+                    header: "My New Table",
+                    keyName: "id",
+                    pageSizeOptions: [ // Pagination options to be applied for the table. All will be shown by default. Pagination will be hidden if this key is missing or empty.
+                        20,
+                        40,
+                        60
+                    ],
+                    defaultPageSize: "20" //Will be ignored if pageSizeOptions is not provided. Pagesize will default to All if this key is missing or empty.
+                },
+                elements:{
+                  columns:[
+                  { field: 'id', headerName: 'ID', width: 90 },
+                  {
+                    field: 'firstName',
+                    headerName: 'First name',
+                    width: 150,
+                    editable: true,
+                  },
+                  {
+                    field: 'lastName',
+                    headerName: 'Last name',
+                    width: 150,
+                    editable: true,
+                  },
+                  {
+                    field: 'age',
+                    headerName: 'Age',
+                    type: 'number',
+                    width: 110,
+                    editable: true,
+                  },
+                  {
+                    field: 'fullName',
+                    headerName: 'Full name',
+                    description: 'This column has a value getter and is not sortable.',
+                    sortable: false,
+                    width: 160,
+                  },
+                ], rows: [
+                  { id: 1, lastName: 'Dsouza', firstName: 'Steven', age: 35 },
+                  { id: 2, lastName: 'Dasari', firstName: 'Sudhakar', age: 42 },
+                  { id: 3, lastName: 'Krishna', firstName: 'Rama', age: 45 },
+                  { id: 4, lastName: 'Stark', firstName: 'Santhosha', age: 16 },
+                ]
+              }
+            }
+          }
+          ]
+        },
+        {
+            label: "Check 1",
+            id: "Identifier for this node.",
+            type: "form",
+            image: "Image name to show for this node",// Should be from a predefined set of images.
+            parentId: "Id of the parent for this node. Omit this key for root nodes."
+        },
+        {
+            label: "Check 2",
+            id: "Identifier for this node.",
+            type: "type of the page",
+            image: "Image name to show for this node",// Should be from a predefined set of images.
+            parentId: "Id of the parent for this node. Omit this key for root nodes."
+        }
     ]
-  },
-  {
-    id: uuid(),
-    name: "Italian",
-    children: [
-      {
-        id: uuid(),
-        name: "Level A",
-        children: []
-      }
-    ]
-  }
-];
+};
 
 export const seasons = [
   {
@@ -36,7 +183,10 @@ export const seasons = [
         children: [
           {
             id: uuid(),
-            name: "June"
+            name: "June",
+            type: "table",
+            rows: [{ id: 1, lastName: 'Snow' },
+            { id: 2, lastName: 'Lannister' }]
           },
           {
             id: uuid(),
@@ -105,3 +255,6 @@ export const seasons = [
     ]
   }
 ];
+
+
+
